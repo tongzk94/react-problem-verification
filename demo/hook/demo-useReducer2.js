4e3e2f466c5d32c523dc6f6b2f5ce479aabe2951
tongzk94 <tongzk365@163.com>
2020-07-26 00:39:16 +0800

    useReducer惰性初始化

---
+++ demo/hook/demo-useReducer2.js
@@ -0,0 +1,41 @@
+import React, { useState, useReducer } from "react";
+
+/**
+ * useReducer惰性初始化
+ */
+// 提出初始化逻辑
+function init(initialCount) {
+  return { count: initialCount };
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    case "reset":
+      // 重置state
+      return init(action.payload);
+    default:
+      throw new Error();
+  }
+}
+
+function TestReducer2({ initialCount }) {
+  const [state, dispatch] = useReducer(reducer, initialCount, init);
+  return (
+    <>
+      Count: {state.count}
+      <button
+        onClick={() => dispatch({ type: "reset", payload: initialCount })}
+      >
+        Reset
+      </button>
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+    </>
+  );
+}
+
+export default TestReducer2;
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import TestState from "./demo/hook/demo-useState";
 // import TestEffect from "./demo/hook/demo-useEffect";
 // import TestRef from "./demo/hook/demo-useRef";
 import TestReducer from "./demo/hook/demo-useReducer";
+import TestReducer2 from "./demo/hook/demo-useReducer2";
 
 class App extends Component {
   constructor() {
@@ -21,6 +22,8 @@ class App extends Component {
         <Hello name={this.state.name} />
         <p>good good study, day day up!</p>
         <p>study hard, improve every day!</p>
+        <TestReducer2 initialCount={0} />
+        <br />
         <TestReducer />
         {/* <TestRef /> */}
         {/* <TestEffect /> */}
